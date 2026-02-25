39fbef64ef0ed42249429317d4e8d4dd6ce7976e
jammmin02 <jmkm1201@naver.com>
2026-02-25 20:44:46 +0900

    구조 변경

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
-let hello:string = "Hello, TypeScript!"
+import App from './App'
+import router from './routes'
 
-console.log(hello)
+const root = document.querySelector('#root')
+root?.append(new App().el)
+
+// 루트 요소를 등록한 후에 실행해야 하는 플러그인!
+router()
